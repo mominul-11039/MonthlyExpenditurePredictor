f15422f56aa98a09ae2e51c2dfed966df5508b99
mostafa.yeasar <mostafa.yeasar@bjitgroup.com>
2023-06-13 11:02:50 +0600

    [UPSkill-Dev][Q4-2023

---
+++ Monthly Expenditure Predictor/Screens/IntialScreen.swift
@@ -0,0 +1,60 @@
+//
+//  IntialScreen.swift
+//  Monthly Expenditure Predictor
+//
+//  Created by BJIT on 13/6/23.
+//
+
+import SwiftUI
+
+struct InitialScreenView: View {
+    @State private var isDiaryVisible = false
+    @State private var isPenVisible = false
+    @State private var isNavigationActive = false
+
+
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.mint.opacity(0.1) // Background color
+                VStack {
+                    Image("bill_prev_ui")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 300, height: 300)
+                        .offset(x: isDiaryVisible ? 0 : -300, y: 0)
+                        .animation(.interpolatingSpring(stiffness: 100, damping: 10))
+                        .opacity(isDiaryVisible ? 1 : 0)
+
+                    NavigationLink(destination: HomeScreen(tabSelection: .home), isActive: $isNavigationActive) {
+                        Button(action: {
+                            isNavigationActive = true
+                        }) {
+                            Text("Record your Expenditure")
+                                .font(.title)
+                                .fontWeight(.medium)
+                                .foregroundColor(.black.opacity(0.5))
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(10)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+            }
+            .edgesIgnoringSafeArea(.all) // Extend the background color to edges
+            .onAppear {
+                withAnimation {
+                    isDiaryVisible = true
+                    isPenVisible = true
+                }
+            }
+        }
+    }
+}
+
+struct InitialScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        InitialScreenView()
+    }
+}
